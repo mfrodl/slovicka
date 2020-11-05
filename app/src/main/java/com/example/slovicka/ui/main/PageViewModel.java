954b639f14052f8c6adac655d20f5bd6a0607490
Martin Frodl <maarilainen@gmail.com>
2020-11-05 22:27:12 +0100

    Edit colors & font, add new words

--- app/src/main/java/com/example/slovicka/ui/main/PageViewModel.java
+++ app/src/main/java/com/example/slovicka/ui/main/PageViewModel.java
@@ -5,8 +5,9 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
-import java.util.stream.IntStream;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -21,11 +22,19 @@ public class PageViewModel extends ViewModel {
         SpannableString spannableString = new SpannableString(text);
         Random randomGenerator = new Random();
 
+        Integer[] colors = {
+                Color.RED,
+                Color.GREEN,
+                Color.BLUE,
+                Color.CYAN,
+                Color.MAGENTA,
+                Color.BLACK
+        };
+
+        Collections.shuffle(Arrays.asList(colors));
+
         for (int i = 0; i < text.length(); i++) {
-            int red = randomGenerator.nextInt(256);
-            int green = randomGenerator.nextInt(256);
-            int blue = randomGenerator.nextInt(256);
-            int color = Color.rgb(red, green, blue);
+            int color = colors[i % colors.length];
             spannableString.setSpan(new ForegroundColorSpan(color), i,i + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
 
